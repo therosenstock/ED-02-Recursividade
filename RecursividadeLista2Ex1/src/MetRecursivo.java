ab17f109e5b96581d60b7c7b46db57ca2540a36f
therosenstock <fabiolars2009@hotmail.com>
2024-03-03 23:05:39 -0300

    initial commit

---
+++ RecursividadeLista2Ex1/src/MetRecursivo.java
@@ -0,0 +1,52 @@
+
+public class MetRecursivo {
+
+	public int multiplicaSoma (int a, int b)
+	{
+		if( b <= 1)
+		return a;
+		return a + multiplicaSoma(a, b-1);
+	}
+
+	public int restoDivisao (int a, int b)
+	{
+		if(b > a)
+		{
+			return a;
+		}
+
+		a = a - b;
+
+		return restoDivisao(a, b);
+	}
+
+	public int contaPares(int[] vet, int tam, int pares)
+	{
+
+		if (tam < 0)
+		{
+			return pares;
+		}
+
+
+		if( vet[tam] % 2 == 0)
+		{
+			pares++;
+		}
+
+
+		return contaPares(vet, tam - 1, pares);
+	}
+
+	public int fatorialDuplo(int a)
+	{
+		if (a <= 1)
+		{
+			return 1;
+		}
+		return a * fatorialDuplo(a - 2);
+	}
+
+
+}
+
---
+++ RecursividadeLista2Ex1/src/Principal.java
@@ -0,0 +1,59 @@
+
+import javax.swing.*;
+public class Principal {
+
+	public static void main(String[] args) {
+	
+	
+	MetRecursivo met = new MetRecursivo();
+	
+	int num1 , num2, opcao;
+	
+	opcao =  Integer.parseInt(JOptionPane.showInputDialog("Menu Principal \n 1 - Multiplicação \n 2 - Divisão \n 3 - Pares \n 4- Fatorial Duplo \n 5- MDC"));
+	
+	switch (opcao) 
+	{
+	case 1:
+		num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número:"));
+		num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número:"));
+		System.out.println(met.multiplicaSoma(num1, num2));
+		break;
+		
+	case 2:
+		num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número:"));
+		num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número:"));	
+		System.out.println(met.restoDivisao(num1, num2));
+		break;
+		
+	
+		
+	case 3:
+		
+		int tamanho = Integer.parseInt(JOptionPane.showInputDialog("Digite o tamanho do vetor:"));
+		int[] vet1 = new int[tamanho];
+		
+		for (int i = 0; i < tamanho; i ++){
+			vet1[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite um número para incluir no vetor:"));
+		}
+		
+		System.out.println(met.contaPares(vet1, tamanho - 1, 0));
+		break;
+		
+	case 4:
+		do {
+			num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o  número para fazer o Fatorial Duplo:"));
+		} 
+		while ((num1 % 2) == 0 );
+		
+		System.out.println(met.fatorialDuplo(num1));
+		break;
+
+	
+	default:
+		System.out.println("Opção Inválida");
+		break;
+	
+	
+	}
+	}
+}
